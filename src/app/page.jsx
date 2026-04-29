1a4c8c922de9635c3eabb90456efb68497ac62b4
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-30 02:04:52 +0600

    added features tiles and show card

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import FeaturesTiles from '@/components/homepage/featuresTiles/FeaturesTiles';
 import Banner from '@/components/shared/banner/Banner';
+import Marqueee from '@/components/homepage/marquee/Marqueee'
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <Marqueee/>
       <FeaturesTiles/>
     </div>
   );
