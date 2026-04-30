b22b7a5823fbb96f68523d82ba44885c1f07f6f4
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-30 12:39:17 +0600

    better auth fixed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.MONGO_DB_URI
+    baseURL: "http://localhost:3000"
 })
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient()
